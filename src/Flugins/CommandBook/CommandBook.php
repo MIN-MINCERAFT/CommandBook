0e0ce94571a68f1f0128c7af54f111664fcf95b9
민 <alsrb4171@gmail.com>
2022-03-11 18:20:23 +0900

    Improve code
    Some codes were duplicated and were fixed together with errors

--- src/Flugins/CommandBook/CommandBook.php
+++ src/Flugins/CommandBook/CommandBook.php
@@ -14,30 +14,29 @@ final class CommandBook extends PluginBase
 {
     protected function onEnable(): void
     {
-        $this->getServer()->getCommandMap()->register('cmb', new CommandBookCommand());
-        $this->getServer()->getPluginManager()->registerEvent(PlayerInteractEvent::class, function (PlayerInteractEvent $event):void
-        {
+        $server = $this->getServer();
+        $commandmap = $server->getCommandMap();
+        $commandmap->register('cmb', new CommandBookCommand());
+        $server->getPluginManager()->registerEvent(PlayerInteractEvent::class, function (PlayerInteractEvent $event) use ($server, $commandmap): void {
             $player = $event->getPlayer();
+            $name = $player->getName();
             $item = $event->getItem();
             $nametag = $item->getNamedTag();
-            if($item->getId() === 403 && $nametag->getString('cmd') !== null)
-            {
-                if($nametag->getInt('rdu'))
-                {
+            if ($item->getId() === 403 && $nametag->getTag('cmd') !== null) {
+                if ($nametag->getInt('rdu')) {
                     $player->getInventory()->setItemInHand($item->setCount($item->getCount() - 1));
                 }
-                if($nametag->getInt('per'))
-                {
-                    $content = str_replace('(name)', $player->getName(), $nametag->getString('cmd'));
-                    if (!$this->getServer()->isOp($player->getName())) {
+                if ($nametag->getInt('per')) {
+                    $content = str_replace('(name)', $name, $nametag->getString('cmd'));
+                    if (!$server->isOp($name)) {
                         $player->setBasePermission(DefaultPermissions::ROOT_OPERATOR, true);
-                        $this->getServer()->getCommandMap()->dispatch($player, $content);
+                        $commandmap->dispatch($player, $content);
                         $player->unsetBasePermission(DefaultPermissions::ROOT_OPERATOR);
-                    }else{
-                        $this->getServer()->getCommandMap()->dispatch($player, $content);
+                    } else {
+                        $commandmap->dispatch($player, $content);
                     }
-                }else{
-                    $this->getServer()->getCommandMap()->dispatch($player, $nametag->getString('cmd'));
+                } else {
+                    $commandmap->dispatch($player, $nametag->getString('cmd'));
                 }
                 $player->sendMessage('§l§a• §r커맨드북을 사용하였어요!');
             }
